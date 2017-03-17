04b10de36e913d2b824504e436ce8ffed8b61ebe
Samuel Pastva <daemontus@gmail.com>
2017-03-17 11:25:28 +0100

    change scale metrics

--- src/main/java/com/github/sybila/biodivine/exe/IdealScale.kt
+++ src/main/java/com/github/sybila/biodivine/exe/IdealScale.kt
@@ -10,13 +10,15 @@ fun main(args: Array<String>) {
     SolverStats.reset(System.out)
     (1..(args[0].toInt())).map {
         thread {
+            val start = System.currentTimeMillis()
             RectangleSolver(rectangleOf(0.0, 10.0, 0.0, 10.0)).run {
-                while (true) {
+                repeat(10000000) {
                     val a = rectangleOf(0.0, 5.0, 0.0, 5.0).asParams()
                     val b = rectangleOf(3.0, 6.0, 2.0, 7.0).asParams()
                     (tt and (a or b).not()).isSat()
                 }
             }
+            println("Done in ${System.currentTimeMillis() - start}")
         }
     }
 }
