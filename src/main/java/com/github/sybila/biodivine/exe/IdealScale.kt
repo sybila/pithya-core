d1c4a50644ef0e6f8b08a01c284a8ad3db432113
Samuel Pastva <daemontus@gmail.com>
2017-03-17 17:17:43 +0100

    allocation throughput

--- src/main/java/com/github/sybila/biodivine/exe/IdealScale.kt
+++ src/main/java/com/github/sybila/biodivine/exe/IdealScale.kt
@@ -10,15 +10,23 @@ fun main(args: Array<String>) {
     SolverStats.reset(System.out)
     (1..(args[0].toInt())).map {
         thread {
+            val limit = 20000
+            var counter = 0L
             val start = System.currentTimeMillis()
-            RectangleSolver(rectangleOf(0.0, 10.0, 0.0, 10.0)).run {
+            while (System.currentTimeMillis() < start + limit) {
+                repeat(100) {
+                    val k = LongArray(4) { counter.also { counter += 1 } }
+                    counter += k.sum()
+                }
+            }
+            /*RectangleSolver(rectangleOf(0.0, 10.0, 0.0, 10.0)).run {
                 repeat(10000000) {
                     val a = rectangleOf(0.0, 5.0, 0.0, 5.0).asParams()
                     val b = rectangleOf(3.0, 6.0, 2.0, 7.0).asParams()
                     (tt and (a or b).not()).isSat()
                 }
-            }
-            println("Done in ${System.currentTimeMillis() - start}")
+            }*/
+            println("Done in ${System.currentTimeMillis() - start} and counter ${counter}")
         }
     }
 }
